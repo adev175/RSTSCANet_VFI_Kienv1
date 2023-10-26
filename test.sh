4ed430db02c419eae7429b6c32d877c4cf340ca9
NAHe <nhatanh.75.got@gmail.com>
2023-10-26 17:06:38 +0900

    add test.py error in module call out

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ CUDA_VISIBLE_DEVICES=0 python test.py \
 --datasetName Vimeo_90K \
 --datasetPath 'data\vimeo_triplet' \
 --checkpoint_dir 'checkpoints\rstscanet.pth' \
---save_path 'checkpoints\try1'
+#--save_path 'result'\
